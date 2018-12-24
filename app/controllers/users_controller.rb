99dd177a8d5f163e61a6841ba9620af9fe3448b3
Sushi Chanrai <sushi@pathworks.co>
2018-12-24 08:57:22 -0500

    build signup route with mentor path logic

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -23,8 +23,31 @@ class UsersController < ApplicationController
   end
 
   get '/signup' do
-    erb :'/users/signup'
+    if
+      !logged_in?
+      erb :'/users/signup'
+    else
+      redirect '/startups'
+    end
   end
+
+  post '/signup' do
+      @user = User.new(name: params[:name],email: params[:email],password: params[:password])
+      if @user.save
+      session[:id] = @user.id
+        if !!params[:yes]
+          redirect "/mentor/new"
+        else
+          redirect "/startups"
+        end
+      else
+      #flash message about signup credentials being invalid, try again
+      #what about if user already exists?
+      redirect '/signup'
+      end
+  end
+
+
 
 
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ActiveRecord::Base
   has_secure_password
   validates :name, presence: true
   validates :email, presence: true
+  validates :password, presence: true
   validates :email, uniqueness: true
   #validates_uniqueness_of :email, message: 'a user already exists with this email'
 
